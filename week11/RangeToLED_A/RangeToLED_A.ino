e60f5d7bf003475a35db6e47bdf76cadff2cfaa5
Maxim Safioulline <maxim@connectedcatmedia.com>
2018-11-03 17:55:09 -0700

    Updated variable name

--- week11/RangeToLED_A/RangeToLED_A.ino
+++ week11/RangeToLED_A/RangeToLED_A.ino
@@ -1,5 +1,5 @@
 // prepare a varaible for the values we'll receive from Processing
-String whateverWeGetFromProcessing;
+String serialReadout;
 // set the LED pin variable to be 3
 int PWM_LEDpin = 3;
 
@@ -15,8 +15,8 @@ void loop() {
   // put your main code here, to run repeatedly:
   if (Serial.available()) { // If data is available to read,
     // read it and store it in our variable
-    whateverWeGetFromProcessing = Serial.read();
+    serialReadout = Serial.read();
     // convert the read value to the integer and write it to the LED pin
-    analogWrite(PWM_LEDpin, whateverWeGetFromProcessing.toInt());
+    analogWrite(PWM_LEDpin, serialReadout.toInt());
   }
 }
